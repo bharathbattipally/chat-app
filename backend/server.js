22da4e0b1215c71eb1f150d42a0d186a839e8a3a
Bharath <bharathbattipally@gmail.com>
2024-02-21 23:10:11 +0530

    2nd part completed

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
+import {app, server} from './socket/socket.js';
 
 import connectToMongoDB from './db/connectToMongoDB.js';
 
@@ -11,7 +13,9 @@ import userRoutes from './routes/user.routes.js'
 
 dotenv.config();
 
-const app=express();
+const __dirname= path.resolve();
+
+
 const PORT= process.env.PORT || 9000;
 
 /*app.get('/',(req,res)=>{
@@ -28,7 +32,13 @@ app.use('/api/auth',authRoutes); //using middleware
 app.use('/api/messages',messageRoutes);
 app.use('/api/users',userRoutes);
 
-app.listen(PORT,()=>{
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
+})
+
+server.listen(PORT,()=>{
     connectToMongoDB();
     console.log(`server running on port ${PORT}`);
 })
